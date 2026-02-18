78b518a183bc28067ba2ed6d260aad1a2f32f3d4
Kone Tshivhinda <erictshivhinda@gmail.com>
2026-02-18 03:00:41 +0200

    Vercel/render deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,8 +6,10 @@
 
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Express Server URL
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
